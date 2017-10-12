b4241275a3f09dd179845069a37f7c810ccb6953
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2017-10-12 12:23:30 +0800

    Update license

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+//  This Source Code Form is subject to the terms of the Mozilla Public
+//  License, v. 2.0. If a copy of the MPL was not distributed with this
+//  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
 const express = require('express')
 const app = module.exports = express()
 const path = require('path')
